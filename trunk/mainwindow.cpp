413af893ce290abc7506ac36bb3fa4ff0e604c94
chris.whiten@gmail.com <chris.whiten@gmail.com@2d489278-39b7-6d3c-1875-f5af8cb81334>
2011-10-18 00:01:34 +0000

    Committing initial QT configurations.  Just getting used to this environment.  Set up the basics of the window.  For next commit, I'd like to get points drawn instead of this line, where the points are all from some point vector.  It would probably be good practice to have a 'point' class...

---
+++ trunk/mainwindow.cpp
@@ -0,0 +1,34 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <iostream>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    QGraphicsScene *scene;
+    QGraphicsView *view = new QGraphicsView(this);
+
+    view->setGeometry(QRect(20, 50, 400, 400));
+    scene = new QGraphicsScene(50, 50, 350, 350);
+    view->setScene(scene);
+
+    QGraphicsLineItem *line = new QGraphicsLineItem(10, 10, 50, 50);
+    line->setFlag((QGraphicsItem::ItemIsMovable));
+    scene->addItem(line);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::numberOfPointsHandler()
+{
+    int num_pts = 0;
+    num_pts = ui->horizontalSlider->value();
+    std::cout << "num_pts = " << num_pts << std::endl;
+}
