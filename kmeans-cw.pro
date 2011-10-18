8607b4608e234f504f1bb464bad2232a3f51ecae
chris.whiten <chris.whiten@gmail.com>
2011-10-18 00:01:34 +0000

    Committing initial QT configurations.  Just getting used to this environment.  Set up the basics of the window.  For next commit, I'd like to get points drawn instead of this line, where the points are all from some point vector.  It would probably be good practice to have a 'point' class...

---
+++ kmeans-cw.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2011-10-17T18:54:14
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+TARGET = kmeans-cw
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
---
+++ mainwindow.h
@@ -0,0 +1,28 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <QGraphicsLineItem>
+
+namespace Ui {
+    class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+public slots:
+    void numberOfPointsHandler();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
