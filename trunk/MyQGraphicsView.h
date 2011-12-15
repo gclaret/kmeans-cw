53563b887997e85ac3c8c97f0c3581c06ca33f53
chris.whiten@gmail.com <chris.whiten@gmail.com@2d489278-39b7-6d3c-1875-f5af8cb81334>
2011-12-15 17:55:15 +0000

    Just committing so I can work at home from where I am.  Still looking into getting CGAL to run to give the Voronoi diagram of output.  Would also like to implement a method to automatically choose k.

--- trunk/MyQGraphicsView.h
+++ trunk/MyQGraphicsView.h
@@ -9,6 +9,18 @@
 #include "PointList.h"
 #include "Cluster.h"
 
+#include <CGAL/Voronoi_diagram_2.h>
+
+// triangulation
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Delaunay_triangulation_2.h>
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
+typedef Kernel::Point_2 Point;
+
+typedef CGAL::Delaunay_triangulation_2<Kernel> Delaunay;
+typedef Delaunay::Vertex_handle Vertex_handle;
+
 class MyQGraphicsView : public QGraphicsView
 {
     Q_OBJECT
@@ -18,6 +30,7 @@ public:
     void colourCluster(Cluster *c);
     QGraphicsScene *getScene();
     void delayedColourClusters(std::vector<Cluster *> &clusters);
+    void delaunayTriangulate(std::vector<Point *> pts);
 
 signals:
 
